a16ca5168518f2eab50c2780e4ba536f5317356a
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2022-01-24 19:32:43 -0800

    01242022-Update

--- js/site.js
+++ js/site.js
@@ -1,4 +1,41 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+//dynamic array object
+var dynamicArray={
+     size:0,
+     index:0,
+     array:new Array(),
+     Init:function(){
+         //Initialize the array with a size given by the user
+         size=4;
+         array=new Array(size);
+     },
+     //Checks if the array is filled with elements , if it is , copies the new elements into the new array
+     CheckIfFull:function(){
+         if (this.array.length==size)
+         {
+             size*=2;
+             newArray= new Array(size)
+             for(let i=0;i<size;i++)
+             {
+                 newArray[i]=this.array[i];
+                 
+             }
+             this.array=newArray;
+             
+
+         }
+    },
+    //Adds a new element to the array
+    AddElement:function(element){
+        this.array[this.index]=element;
+        this.index++;
+        this.CheckIfFull();
+    }
 }
+
+
+dynamicArray.Init();
+dynamicArray.AddElement('Apple');
+dynamicArray.AddElement('Orange');
+dynamicArray.AddElement('Strawberry');
+dynamicArray.AddElement('Coconut');
+dynamicArray.AddElement('Mango');
